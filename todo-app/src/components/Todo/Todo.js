bad48807db9f94884620f8474fb66b021f904eae
Sebastienbezombes <bezombessebastien@gmail.com>
2020-05-31 11:32:45 +0200

    init modify checkboxs but difficulties to recup element ::after in JavaScript

--- todo-app/src/components/Todo/Todo.js
+++ todo-app/src/components/Todo/Todo.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react'
 import './Todo.css'
 
-
 export default class Todo extends Component {
+
+    check = () => {
+        let checking = document.querySelector("input::after")
+        checking.style.background = "green"
+    }
+
     render() {
         return (
             <div class="Todo">
                 <h3>Nom de la tâche</h3> 
-               <input class="bg-red" type="checkbox"/>
+               <input type="checkbox" onClick={this.check}/>
             </div>
         )
     }
