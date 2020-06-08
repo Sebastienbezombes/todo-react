2a75f62828b85bbc99c52b68aa82be39dba1b2ff
Sebastienbezombes <bezombessebastien@gmail.com>
2020-06-08 18:07:40 +0200

    delete todo

--- todo-app/src/components/TodoList/TodoList.js
+++ todo-app/src/components/TodoList/TodoList.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react'
-import Todo from '../Todo/Todo'
 import './TodoList.css'
 
 export default class TodoList extends Component {
     render() {
         return (
             <div className="TodoList">
-                <Todo/>
-                <Todo/>
-                <Todo/>
-                <Todo/>
-                <Todo/>
+
             </div>
         )
     }
